22e5a33477b62f1aed87a81a22948fd168b22b53
Brad Beggs <brad.beggs@gmail.com>
2020-07-01 20:58:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,9 @@ def reverse_each_word(string_to_reverse)
   holding_array = []
   holding_array << string_to_reverse.split
   print holding_array[0]
-  #string_to_reverse.each do |backwards_word|
-  #  print backwards_word
-  #end
+  holding_array.each do |backwards_word|
+    print backwards_word
+  end
 
 
 end
