9eb9703553f09742d2471753aed222eda0568d5c
Brad Beggs <brad.beggs@gmail.com>
2020-07-01 21:15:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,9 @@
 def reverse_each_word(string_to_reverse)
   holding_array = string_to_reverse.split(' ')
+  backwards_word_sentence
 
   holding_array.each do |backwards_word|
-    print backward_setence = backwards_word.reverse
+    print backwards_word_sentence = backwards_word.reverse
   end
 
 
